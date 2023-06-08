35ccd531f92551d6cee96af51b75a531d626e282
TheJoker237 <leon.timo.3182@gmail.com>
2023-06-08 11:07:28 +0100

    new

---
+++ app/Mail/TestMail.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailables\Attachment;
+use PhpParser\Node\Expr\Cast\Array_;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $data = [];
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Array $user)
+    {
+        $this->data = $user;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('thejoker@mailcom', 'The Joker'),
+            subject: 'Test Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.test',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [
+            Attachment::fromStorage('img/skull.jpg')
+        ];
+    }
+}
---
+++ resources/views/emails/markdown-test.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Introduction
+
+Hello les gars
+
+<x-mail::button :url="$url">
+Cliquez ici
+</x-mail::button>
+
+Merci,<br>
+{{ config('app.name') }}
+</x-mail::message>
